595fe6509645209216717f19d6314a4ed7827845
bedjo <obedindevu@gmail.com>
2023-02-14 22:54:07 +0200

    feat: create components

---
+++ exotailwindwreact/src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Sidebar() {
+  return (
+    <div className='container'>
+        <div className='boddySide'>
+            <p>Suis dans le sidebar</p>
+        </div>
+
+    </div>
+  )
+}
